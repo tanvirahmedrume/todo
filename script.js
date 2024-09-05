6b52a507e7b8e4daafcea3429a3199611c8379d9
Tanvir <tstanvir371806@gmail.com>
2024-09-05 23:21:49 +0600

    a

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById('inputBox');
+const listContainer = document.getElementById('list-container');
+
+function addTask(event){
+    
+    if (inputBox.value === '') {
+        alert('You must write somethin');
+    }else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }    
+    inputBox.value = '';
+    saveData();
+    event.preventDefault();
+}
+
+listContainer.addEventListener('click', function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle('checked');
+        saveData();
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+function saveData(){
+    localStorage.setItem("Data", listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("Data");
+}
+
+showTask();
